ce01ba625ca1ef847c513ac722e5ac0db4c6f5ff
anfield-laptop <zhp2019@mail.ustc.edu.cn>
2022-07-21 23:54:59 +0800

    将tello的姿态与noop的位置融合得到tello的位姿

--- src/trans2mavros/nodeframe1_to_odom1.cpp
+++ src/trans2mavros/nodeframe1_to_odom1.cpp
@@ -1,6 +1,7 @@
 #include <nlink_parser/LinktrackNodeframe1.h>
 #include <ros/ros.h>
 #include <nav_msgs/Odometry.h>
+#include <tf/transform_datatypes.h>
 
 nav_msgs::Odometry odometry_msg;
 
@@ -8,19 +9,28 @@ void nodeFrame1Callback(const nlink_parser::LinktrackNodeframe1 &msg){
     odometry_msg.header.stamp = ros::Time::now();
     odometry_msg.pose.pose.position.x = msg.nodes[0].pos_3d[0];
     odometry_msg.pose.pose.position.y = msg.nodes[0].pos_3d[1];
-    odometry_msg.pose.pose.position.z = 0.0;
-    odometry_msg.pose.pose.orientation.w = 1.0;
-    odometry_msg.pose.pose.orientation.x = 0.0;
-    odometry_msg.pose.pose.orientation.y = 0.0;
-    odometry_msg.pose.pose.orientation.z = 0.0;
+    odometry_msg.pose.pose.position.z = 1.0;
+}
+
+void telloOdomCallback(const nav_msgs::Odometry & msg){
+    tf::Quaternion quat;
+    tf::quaternionMsgToTF(msg.pose.pose.orientation, quat);
+    double r,p,y;
+    tf::Matrix3x3(quat).getRPY(r, p, y);
+    r=0.0;
+    p=0.0;
+    y = -y;
+    odometry_msg.pose.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(r,p,y);
+    // odometry_msg.pose.pose.orientation = msg.pose.pose.orientation
 }
 
 int main(int argc, char **argv){
   ros::init(argc, argv, "frame1_to_mavros");
   ros::NodeHandle nh;
-  ros::Rate loop_rate(100);
+  ros::Rate loop_rate(20);
   ros::Publisher odometry_pub = nh.advertise<nav_msgs::Odometry>("odometry1", 10);
-  ros::Subscriber frame2_data = nh.subscribe("nlink_linktrack_nodeframe1", 10, &nodeFrame1Callback);
+  ros::Subscriber frame1_data = nh.subscribe("nlink_linktrack_nodeframe1", 10, &nodeFrame1Callback);
+  ros::Subscriber odom_sub = nh.subscribe("/tello/odom", 10, &telloOdomCallback);
 
   while (ros::ok()){
         odometry_pub.publish(odometry_msg);
